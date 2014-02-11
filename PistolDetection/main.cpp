a37ca2906a0eb444b1bb05a2de387ce414255120
John Doherty <john.doherty13@gmail.com>
2014-02-10 21:40:23 -0800

    Initial Commit

---
+++ PistolDetection/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  PistolDetection
+//
+//  Created by John Doherty on 2/10/14.
+//  Copyright (c) 2014 John Doherty, Aaron Damashek. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+

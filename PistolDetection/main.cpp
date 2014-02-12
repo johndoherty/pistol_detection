6bed60c38ed495cd1ac9d7262eb4f8166d95594c
Aaron Damashek <aarond@stanford.edu>
2014-02-11 22:39:21 -0800

    Put in basic Chamfer Code

--- PistolDetection/main.cpp
+++ PistolDetection/main.cpp
@@ -5,7 +5,7 @@
 //  Created by John Doherty on 2/10/14.
 //  Copyright (c) 2014 John Doherty, Aaron Damashek. All rights reserved.
 //
-
+/*
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
@@ -25,10 +25,10 @@ int ratio = 3;
 int kernel_size = 3;
 char* window_name = "Edge Map";
 
-/**
- * @function CannyThreshold
- * @brief Trackbar callback - Canny thresholds input with a ratio 1:3
- */
+
+ //@function CannyThreshold
+ //brief Trackbar callback - Canny thresholds input with a ratio 1:3
+
 void CannyThreshold(int, void*)
 {
     /// Reduce noise with a kernel 3x3
@@ -45,11 +45,10 @@ void CannyThreshold(int, void*)
 }
 
 
-/** @function main */
 int main( int argc, char** argv )
 {
     /// Load an image
-    src = imread("/Users/john/Dropbox/School/CS231a/Project/PistolDetection/PistolDetection/X077_03.jpg");
+    src = imread("/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/X077_03.jpg");
     
     if( !src.data )
     { return -1; }
@@ -74,4 +73,64 @@ int main( int argc, char** argv )
     
     return 0;
 }
+*/
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/contrib/contrib.hpp"
+#include <stdlib.h>
+#include <stdio.h>
 
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+void help()
+{
+	cout <<
+    "\nThis program demonstrates chamfer matching -- computing a distance between an \n"
+    "edge template and a query edge image.\n"
+    "Call:\n"
+    "./chamfermatching [<image edge map> <template edge map>]\n"
+    "By default\n"
+    "the inputs are ./chamfermatching logo_in_clutter.png logo.png\n"<< endl;
+}
+int main( int argc, char** argv )
+{
+    if( argc != 1 && argc != 3 )
+    {
+        help();
+        return 0;
+    }
+    Mat img = imread(argc == 3 ? argv[1] : "logo_in_clutter.png", 0);
+    Mat cimg;
+    cvtColor(img, cimg, CV_GRAY2BGR);
+    Mat tpl = imread(argc == 3 ? argv[2] : "logo.png", 0);
+    
+    // if the image and the template are not edge maps but normal grayscale images,
+    // you might want to uncomment the lines below to produce the maps. You can also
+    // run Sobel instead of Canny.
+    
+    Canny(img, img, 5, 50, 3);
+    Canny(tpl, tpl, 5, 50, 3);
+    
+    vector<vector<Point> > results;
+    vector<float> costs;
+    int best = chamerMatching( img, tpl, results, costs );
+    if( best < 0 )
+    {
+        cout << "not found;\n";
+        return 0;
+    }
+    
+    size_t i, n = results[best].size();
+    for( i = 0; i < n; i++ )
+    {
+        Point pt = results[best][i];
+        if( pt.inside(Rect(0, 0, cimg.cols, cimg.rows)) )
+            cimg.at<Vec3b>(pt) = Vec3b(0, 255, 0);
+    }
+    imshow("result", cimg);
+    waitKey();
+    return 0;
+}

64a1c3840183c0c2e040a2f7ea8b70bfdeffb983
John Doherty <john.doherty13@gmail.com>
2014-02-17 00:00:28 -0800

    added demo images and testing code

--- PistolDetection/main.cpp
+++ PistolDetection/main.cpp
@@ -5,83 +5,14 @@
 //  Created by John Doherty on 2/10/14.
 //  Copyright (c) 2014 John Doherty, Aaron Damashek. All rights reserved.
 //
-/*
-#include "opencv2/imgproc/imgproc.hpp"
-#include "opencv2/highgui/highgui.hpp"
-#include <stdlib.h>
-#include <stdio.h>
+//#include "chamfer.h"
 
-using namespace cv;
-
-/// Global variables
-
-Mat src, src_gray;
-Mat dst, detected_edges;
-
-int edgeThresh = 1;
-int lowThreshold;
-int const max_lowThreshold = 100;
-int ratio = 3;
-int kernel_size = 3;
-char* window_name = "Edge Map";
-
-
- //@function CannyThreshold
- //brief Trackbar callback - Canny thresholds input with a ratio 1:3
-
-void CannyThreshold(int, void*)
-{
-    /// Reduce noise with a kernel 3x3
-    blur( src_gray, detected_edges, Size(3,3) );
-    
-    /// Canny detector
-    Canny( detected_edges, detected_edges, lowThreshold, lowThreshold*ratio, kernel_size );
-    
-    /// Using Canny's output as a mask, we display our result
-    dst = Scalar::all(0);
-    
-    src.copyTo( dst, detected_edges);
-    imshow( window_name, dst );
-}
-
-
-int main( int argc, char** argv )
-{
-    /// Load an image
-    src = imread("/Users/john/Dropbox/School/CS231a/Project/pistol_detection/PistolDetection/X077_03.jpg");
-    
-    if( !src.data )
-    { return -1; }
-    
-    /// Create a matrix of the same type and size as src (for dst)
-    dst.create( src.size(), src.type() );
-    
-    /// Convert the image to grayscale
-    cvtColor( src, src_gray, CV_BGR2GRAY );
-    
-    /// Create a window
-    namedWindow( window_name, CV_WINDOW_AUTOSIZE );
-    
-    /// Create a Trackbar for user to enter threshold
-    createTrackbar( "Min Threshold:", window_name, &lowThreshold, max_lowThreshold, CannyThreshold );
-    
-    /// Show the image
-    CannyThreshold(0, 0);
-    
-    /// Wait until user exit program by pressing a key
-    waitKey(0);
-    
-    return 0;
-}
-*/
-
-#include "chamfer.h"
+#include "opencv2/contrib/contrib.hpp"
 #include "opencv2/imgproc/imgproc.hpp"
 #include "opencv2/highgui/highgui.hpp"
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
-
 #include <iostream>
 
 using namespace cv;
@@ -97,84 +28,93 @@ void help()
     "By default\n"
     "the inputs are ./chamfermatching logo_in_clutter.png logo.png\n"<< endl;
 }
-/*int main( int argc, char** argv )
-=======
 
-int main( int argc, char** argv )
->>>>>>> 913e5df3af9fbd014e2bf225da24018b8a2ef41d
-{
-    if( argc != 1 && argc != 3 )
-    {
+int main( int argc, char** argv ) {
+    
+    if( argc != 1 && argc != 3 ) {
         help();
         return 0;
     }
-  */
-/*
-    Mat img = imread(argc == 3 ? argv[1] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/logo_in_clutter.png", CV_LOAD_IMAGE_GRAYSCALE);
-    Mat tpl = imread(argc == 3 ? argv[2] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/logo.png", CV_LOAD_IMAGE_GRAYSCALE);
-    */
-    /*
-    Mat img = imread(argc == 3 ? argv[1] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/X077_03.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    Mat tpl = imread(argc == 3 ? argv[2] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/X077_03.jpg", CV_LOAD_IMAGE_GRAYSCALE);*/
     
-   // Mat img = imread(argc == 3 ? argv[1] : "/Users/john/Dropbox/School/CS231a/Project/pistol_detection/PistolDetection/logo_in_clutter.png", CV_LOAD_IMAGE_GRAYSCALE);
-//    Mat tpl = imread(argc == 3 ? argv[2] : "/Users/john/Dropbox/School/CS231a/Project/pistol_detection/PistolDetection/logo.png", CV_LOAD_IMAGE_GRAYSCALE);
-
-    //Mat img = imread(argc == 3 ? argv[1] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/logo_in_clutter.png", CV_LOAD_IMAGE_GRAYSCALE);
-    //Mat tpl = imread(argc == 3 ? argv[2] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/logo.png", CV_LOAD_IMAGE_GRAYSCALE);
-
-    //Mat img = imread(argc == 3 ? argv[1] : "./X077_03.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-    //Mat tpl = imread(argc == 3 ? argv[2] : "./X077_03.jpg", CV_LOAD_IMAGE_GRAYSCALE);
-
+    /*
+     Mat img = imread(argc == 3 ? argv[1] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/logo_in_clutter.png", CV_LOAD_IMAGE_GRAYSCALE);
+     Mat tpl = imread(argc == 3 ? argv[2] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/logo.png", CV_LOAD_IMAGE_GRAYSCALE);
+     */
+    /*
+     Mat img = imread(argc == 3 ? argv[1] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/X077_03.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+     Mat tpl = imread(argc == 3 ? argv[2] : "/Users/aarondamashek/CS231A/pistol_detection/PistolDetection/X077_03.jpg", CV_LOAD_IMAGE_GRAYSCALE);*/
+    
+    Mat img = imread(argc == 3 ? argv[1] : "/Users/john/Dropbox/School/CS231a/Project/pistol_detection/PistolDetection/pistol_2.jpg", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat cimg;
+    cvtColor(img, cimg, CV_GRAY2BGR);
+    //Mat tpl = imread(argc == 3 ? argv[2] : "/Users/john/Dropbox/School/CS231a/Project/pistol_detection/PistolDetection/logo.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+    Mat tpl = imread(argc == 3 ? argv[1] : "/Users/john/Dropbox/School/CS231a/Project/pistol_detection/PistolDetection/pistol_black_small.jpeg", CV_LOAD_IMAGE_GRAYSCALE);
+    
     // if the image and the template are not edge maps but normal grayscale images,
     // you might want to uncomment the lines below to produce the maps. You can also
     // run Sobel instead of Canny.
+    
+    Canny(img, img, 70, 300, 3);
+    Canny(tpl, tpl, 150, 500, 3);
 
-  //  Canny(img, img, 5, 50, 3);
-   // Canny(tpl, tpl, 5, 50, 3);
+    imshow( "img", img );
+    imshow( "template", tpl );
+    waitKey(0);
+    destroyAllWindows();
+    
+    imshow( "img", img );
+    waitKey(0);
+    destroyAllWindows();
 
-    //imshow( "img", img );
-    //imshow( "template", tpl );
-    //waitKey(0);
-
-
-    //Canny(img, img, 150, 200, 3);
-    //Canny(tpl, tpl, 150, 200, 3);
-
-/*
- char* window_name = "Edge Map";
- namedWindow( window_name, CV_WINDOW_AUTOSIZE );
- imshow(window_name, img);
-*/
-    //char * dir = getcwd(NULL, 0);
-    //cout << "Current dir: " << dir << endl;
-
-
-  //  vector<vector<Point> > results;
-   // vector<float> costs;
-//    int best = chamerMatching( img, tpl, results, costs );
-
-  //  int best = chamerMatching(img, tpl, results, costs);
-    //vector<vector<Point> > results;
-    //vector<float> costs;
-    //int best = chamerMatching(img, tpl, results, costs );
-/*
-    if( best < 0 )
-    {
+    vector<vector<Point> > results;
+    vector<float> costs;
+    
+    /*
+     int chamerMatching( Mat& img, Mat& templ,
+     CV_OUT vector<vector<Point> >& results, CV_OUT vector<float>& cost,
+     double templScale=1, int maxMatches = 20,
+     double minMatchDistance = 1.0, int padX = 3,
+     int padY = 3, int scales = 5, double minScale = 0.6, double maxScale = 1.6,
+     double orientationWeight = 0.5, double truncate = 20);
+     */
+    
+    int best = chamerMatching(img, tpl, results, costs, 1, 50, 1.0, 3, 3, 5, 0.6, 1.6, 0.5, 20);
+    //int best = chamerMatching(img, tpl, results, costs);
+    if( best < 0 ) {
         cout << "not found;\n";
         return 0;
     }
+    size_t j,m = results.size();
+    //size_t j = best;
+    for(j = 0; j < m; j++) {
+        //size_t i, n = results[best].size();
+        size_t i, n = results[j].size();
+        for( i = 0; i < n; i++ ) {
+            Point pt = results[j][i];
+            if(pt.inside(Rect(0, 0, cimg.cols, cimg.rows))) {
+                if (i == best) {
+                    cimg.at<Vec3b>(pt) = Vec3b(255, 0, 0);
+                } else {
+                    cimg.at<Vec3b>(pt) = Vec3b(0, 255, 0);
+                }
+            }
+            
+        }
+    }
     
-    size_t i, n = results[best].size();
-    for( i = 0; i < n; i++ )
-    {
-        Point pt = results[best][i];
-        if( pt.inside(Rect(0, 0, cimg.cols, cimg.rows)) )
-            cimg.at<Vec3b>(pt) = Vec3b(0, 255, 0);
+    cout << "Best index: ";
+    cout << best;
+    cout << "\n";
+    cout << "With cost: ";
+    cout << costs[best];
+    cout << "\n\n";
+    
+    for (int i = 0; i < costs.size(); i++) {
+        cout << costs[i];
+        cout << ", ";
     }
     imshow("result", cimg);
-*/
-   // waitKey();
-   // return 0;
-//}
-
+    imshow("edges", img);
+    waitKey();
+    return 0;
+}
